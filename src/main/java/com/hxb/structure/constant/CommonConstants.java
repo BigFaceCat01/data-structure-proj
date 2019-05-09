985ae14336354257c74b58ad43522fbc3429dd17
huangxb <18228033207@163.com>
2019-05-09 15:25:38 +0800

    增加JWT工具

--- src/main/java/com/hxb/structure/constant/CommonConstants.java
+++ src/main/java/com/hxb/structure/constant/CommonConstants.java
@@ -1,5 +1,8 @@
 package com.hxb.structure.constant;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author create by huang xiao bao
  * @date 2019-04-20 17:07:39
@@ -9,6 +12,10 @@ public final class CommonConstants {
      * 16 进制
      */
     public static final int HEX = 16;
+    /**
+     * 30分钟对应的毫秒数
+     */
+    public static final int THIRTY_MINUTES_MILLISECOND = 30*60*1000;
     public static final String STRING_ZERO = "0";
     public static final String MD5 = "MD5";
     public static final String DOT = ".";
@@ -17,6 +24,14 @@ public final class CommonConstants {
     public static final String PROTOCOL_JAR = "jar";
     public static final String DOT_CLASS = ".class";
     public static final String EMPTY_STRING = "";
-    public static final int DOT_CLASS_LENGTH = DOT_CLASS.length();
+    public static final String JWT_SECRET = "23#Acv(!468ME@kjhJHKlj.0000";
+    public static final String JWT_CONTENT = "sub";
+    public static final String JWT_EXPIRE = "exp";
+    public static final Map<String,Object> JWT_HEADER = new HashMap<>();
+
+    static {
+        JWT_HEADER.put("alg", "HS256");
+        JWT_HEADER.put("typ", "JWT");
+    }
 
 }
---
+++ src/main/java/com/hxb/structure/util/JwtUtils.java
@@ -0,0 +1,121 @@
+package com.hxb.structure.util;
+
+import com.hxb.structure.constant.CommonConstants;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Created by huang xiao bao
+ * @date 2019-05-09 10:02:07
+ */
+@Slf4j
+public class JwtUtils {
+    /**
+     * 签发JWT,基于java-jwt依赖
+     * @param content JSON数据字符串
+     * @param expire token 过期时间
+     * @param unit 时间单位
+     * @return  String
+     *
+     */
+    public static String createJWT(String content, long expire, TimeUnit unit) {
+        long nowMillis = System.currentTimeMillis();
+        long timeout = nowMillis + (expire <= 0 ? CommonConstants.THIRTY_MINUTES_MILLISECOND : TimeUnit.MILLISECONDS.convert(expire,unit));
+        Date currentTime = new Date(nowMillis);
+        Date expireTime = new Date(timeout);
+        Claims claims = Jwts.claims();
+        claims.setSubject(content);
+        claims.setExpiration(expireTime);
+        claims.setIssuedAt(currentTime);
+        // build token
+        return Jwts.builder()
+                // jwt 头信息
+                .setHeader(CommonConstants.JWT_HEADER)
+                // jwt 消息体
+                .setClaims(claims)
+                // jwt 生成时间
+                .setIssuedAt(currentTime)
+                // jwt 过期时间
+                .setExpiration(expireTime)
+                // jwt 签名
+                .signWith(SignatureAlgorithm.HS512,CommonConstants.JWT_SECRET)
+                .compact();
+    }
+
+
+    /**
+     * 验证Token正确性
+     * @param token token
+     * @return true or false
+     */
+    public static boolean checkToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(CommonConstants.JWT_SECRET).parseClaimsJws(token);
+            return true;
+        }catch (SignatureException e){
+            log.error("token = {} 签名可能被窜改",token,e);
+        }catch (ExpiredJwtException e){
+            log.error("token = {} 已过期",token,e);
+        }catch (Exception e) {
+            log.error("token = {} 不合法",token,e);
+        }
+        return false;
+    }
+
+    /**
+     * 获得jwt 消息体
+     * @param token token
+     */
+    public static void tokenInfo(String token,TokenParseCallback callback) {
+        Jws<Claims> claimsJws = null;
+        try {
+            claimsJws = Jwts.parser().setSigningKey(CommonConstants.JWT_SECRET).parseClaimsJws(token);
+        } catch (Exception e) {
+            log.error("token = {} 校验失败",token,e);
+        }
+        callback.tokenContent(claimsJws.getBody().getSubject());
+        callback.tokenExpire(claimsJws.getBody().getExpiration());
+    }
+
+    /**
+     * token解析回调
+     */
+    public interface TokenParseCallback{
+        /**
+         * token 消息体回调
+         * @param content 消息体
+         */
+        void tokenContent(String content);
+
+        /**
+         * token 过期时间回调
+         * @param date 时间
+         */
+        void tokenExpire(Date date);
+    }
+
+    public static void main(String[] args) {
+        String jwt = createJWT("{\"userId\":\"1238464646\"}", -1, TimeUnit.SECONDS);
+        System.out.println(jwt);
+        System.out.println(checkToken(jwt));
+        JwtUtils.tokenInfo(jwt,new TokenParseCallback(){
+
+            @Override
+            public void tokenContent(String content) {
+                System.out.println(content);
+
+            }
+
+            @Override
+            public void tokenExpire(Date date) {
+                System.out.println(date);
+            }
+        });
+    }
+
+
+    private JwtUtils(){}
+}
